edd3f16950aab60e8eb94fc97e990e4f8f357465
Rebecca Bracho <rebeccabracho05@gmail.com>
2023-10-19 13:04:16 -0400

    feat: Initial Structure

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+struct Cuenta {
+    double ci;
+    string client;
+    double account_number;
+    char account_type;
+    char suspend;
+
+}cliente[2000];
+
+int main(){
+    
+}
