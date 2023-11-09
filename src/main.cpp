ebc6f52daa6c561ea0d94eadc405881288644f03
Rebecca Bracho <rebeccabracho05@gmail.com>
2023-11-09 09:43:55 -0400

    feat: menu

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,9 @@
+/* 
+1.-Visualizacion de los datos de clientes y sus cuentas de ahorro o corriente
+2.-Establecer un buscador por codigo de cuenta, nombre del cliente o cedula
+6.-Suspender cuentas de clientes y dar aviso de ello
+
+*/
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -12,6 +18,19 @@ struct Cuenta {
 
 }cliente[2000];
 
+ifstream data_client("C:/Users/HOME/Desktop/trabajos/programas/ProyectoLaboratorio_RG/datos/clients.csv");
+
 int main(){
-    
+
+    cout << "|      MENU       |" << endl;
+    cout << "1.- Cuentas clientes" << endl;
+    cout << "2.- Busqueda de clientes" << endl;
+
+    if(!data_client.is_open()){
+        cout << "El archivo no ha sido encontrado" << endl;
+    }
+
+
+
+
 }
